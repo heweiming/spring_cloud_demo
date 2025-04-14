fcd79b848d2bf173bbb126bac80787de7962c0f5
heweiming <tohe@qq.com>
2025-04-14 09:40:19 +0800

    save version

---
+++ sample_c/src/main/java/com/example/eureka_client2/CSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka_client2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CSampleApplication.class, args);
+	}
+
+}
